7b1c9703d18627330d63b26150ee1fb9b5e56685
Carin Barber <cbarber2289@gmail.com>
2025-10-29 15:09:01 -0500

    -practical exam 2

--- site.js
+++ site.js
@@ -11,12 +11,53 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 ;(async () => {
 
 	// todo: create your "getNextQuestion" function
-
+	const getNextQuestion = async () => {
+		const url = ('https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple')
+		const response = await fetch(url)
+		const json = await response.json()
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		const answers = shuffle([ ...incorrect, correct ])
+		//console.log({ question, answers, correct })
+		return { question, answers, correct }
+	}
+	
 	// todo: create your "renderQuestion" function
-
+	const renderQuestion = ({ question, answers, correct }) => {
+		questionElement.textContent = decodeHtml(question)
+		answersElement.innerHTML = ''
+		answers.forEach(answer => {
+			const btn = document.createElement('button')
+			btn.textContent = decodeHtml(answer)
+			btn.addEventListener('click', async () => {
+				if (answer === correct) {
+					btn.classList.add('correct')
+					answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+					alert('Correct!')
+					return
+				} else {
+					btn.disabled = true
+					alert('Incorrect!')
+				}
+			})
+			answersElement.appendChild(btn)
+		})
+	}
+	//const { question, answers, correct } = await getNextQuestion()
+	//renderQuestion({ question, answers, correct })
 	// todo: add the event listener to the "nextQuestion" button
+	nextQuestionElement.addEventListener('click', async () => {
+		renderQuestion(await getNextQuestion())
+		nextQuestionElement.disabled = true
+		setTimeout(() => nextQuestionElement.disabled = false, 10000);
+	})
 
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
-nextQuestionElement.click()
+ nextQuestionElement.click()
+
+// { 
+// 	question: "",
+// 	answers: [],
+// 	correct: ""
+// }
